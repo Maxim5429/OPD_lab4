e382513c1f39d3331686ea42b53e37d8ad16c31c
Maxim_Petrov <max@itproger.com>
2023-05-24 22:27:08 +0600

    добавил тестирование регистрации

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ from flask import Flask, render_template, request
 app = Flask(__name__) #создание объекта класса Flask
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("index.html")
-
-@app.route('/', methods=['post', 'get'])
+@app.route('/', methods=['post', 'get']) # вход
 def form():
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
-        with open('login.txt','r') as file:
-            f=file.readlines()
-        admin_login = f[0].strip()
-        admin_password = f[1].strip()
-        if (login==admin_login) and (password==admin_password):
-            return("correct")
-        else:
-            return("incorrect")
-
+        with open('login.txt', 'r') as f:
+            for line in f:
+                admin_login, admin_password = line.strip().split(":")
+                if (login==admin_login) and (password==admin_password):
+                    return("Вы зашли в свой профиль")
+            else:
+                return("Неверный логин или пароль")
+@app.route('/register')
+def register():
+    return render_template("registration.html")
+@app.route('/register', methods=['post', 'get']) # регистрация
+def reg():
+    if request.method == 'POST':
+        login = request.form.get('login')
+        password = request.form.get('password')
+        with open('login.txt', 'r') as file:   # проверка, занят ли данный логин
+            for line in file:
+                admin_login, admin_password = line.strip().split(":")
+                if login==admin_login:
+                    return "данный логин уже существует"
+        with open('login.txt','a+') as file:
+            file.write(f"{login}:{password}\n")
+            return "регистрация прошла успешно!"
 if __name__ == '__main__': #запуск
     app.run(debug=True)
--- test.py
+++ test.py
@@ -13,15 +13,24 @@ class Test(unittest.TestCase): # создаем класс для тестиро
     def test_false(self): # тестируем на ввод неверного логина и пароля
         test = app.test_client(self) # создаем клиент для тестирования
         response = test.post(data={'login':'maxim@google.com','password':'njnc78'}) # делаем POST-запрос,передаём неверные логин и пароль
-        self.assertEqual('incorrect',response.text) # ответ, полученный в response должен быть равен 'incorrect'
+        self.assertEqual('Неверный логин или пароль',response.text) # ответ, полученный в response должен быть равен 'Неверный логин или пароль'
     def test_true(self): # тестируем на ввод правильного логина и пароля
         test = app.test_client(self) # создаем клиент для тестирования
         with open('login.txt','r') as file: # открываем файл
             f=file.readlines() # получаем список строк из файла
-        admin_login = f[0].strip() # присваиваем admin_login 1-ую строку, удаляем пробелы
-        admin_password = f[1].strip() # присваиваем admin_password 2-ую строку, удаляем пробелы
+        admin_login, admin_password = f[2].strip().split(":") # получаем правильные логин и пароль в переменные admin_login и admin_password
         response = test.post(data={'login':admin_login,'password':admin_password})  # делаем POST-запрос,передаём верные логин и пароль
-        self.assertEqual('correct',response.text) # ответ, полученный в response должен быть равен 'correct'
-
+        self.assertEqual('Вы зашли в свой профиль',response.text) # ответ, полученный в response должен быть равен 'Вы зашли в свой профиль'
+    def test_register(self): # проверка того, что нельзя зарегистрироваться с уже существующим логином
+        test = app.test_client(self)  # создаем клиент для тестирования
+        response = test.post('/register',data={'login':'Artem20@yandex.ru','password': 'njnc78'})  # делаем POST-запрос,передаём уже существующий логин
+        self.assertEqual('данный логин уже существует',response.text)  # ответ, полученный в response должен быть равен 'данный логин уже существует'
+    def test_reg(self): # проверка регистрации
+        test = app.test_client(self)  # создаем клиент для тестирования
+        response = test.post('/register', data={'login': 'Evgeniy@gmail.com','password': 'rd65G7'})  # делаем POST-запрос,передаём логин и пароль для регистрации
+        with open('login.txt', 'r') as file: # открываем файл
+            line=file.readlines()[-1].strip() # считываем последнюю строку в line
+        self.assertEqual("регистрация прошла успешно!",response.text) # ответ, полученный в response должен быть равен 'регистрация прошла успешно!'
+        self.assertEqual(line,'Evgeniy@gmail.com:rd65G7')  # последняя строка из файла login.txt должна равняться 'Evgeniy@gmail.com:rd65G7'
 if __name__ == '__main__': # при условии запуска скрипта
     unittest.main() # запускаем все тесты
